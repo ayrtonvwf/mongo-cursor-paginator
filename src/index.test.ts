2e75a452371c92d2ca87df914d73b101f6e6afa8
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2021-01-27 22:48:22 -0300

    feat: test if the default export is equal to the named export
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { helloWorld } from './index';
+import defaultHelloWorld, { helloWorld } from './index';
 
 import * as mocha from 'mocha';
 import * as chai from 'chai';
@@ -7,5 +7,11 @@ const expect = chai.expect;
 describe('The Hello World function', () => {
   it('should greet us with' , () => {
     expect(helloWorld()).to.equal('Hello World');
+  });
+});
+
+describe('The default export', () => {
+  it('should be equal to the named export', () => {
+    expect(helloWorld).to.equal(defaultHelloWorld)
   });
 });
