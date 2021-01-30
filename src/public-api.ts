9245d0b36112913cd00c9494f9e51f70a86e2db0
Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>
2021-01-29 21:48:17 -0300

    feat: create api definition
    Signed-off-by: Ayrton Vargas Witcel Fidelis <ayrton.vargas33@gmail.com>

---
+++ src/public-api.ts
@@ -0,0 +1,44 @@
+import { MongoClient } from 'mongodb';
+
+export type ForwardConnectionParams = {
+  first: number
+  after?: string|null|undefined
+}
+
+export type BackwardsConnectionParams = {
+  last: number
+  before?: number|null|undefined
+}
+
+export type ConnectionParams = ForwardConnectionParams | BackwardsConnectionParams;
+
+export type Edge<T> = {
+  cursor: string
+  node: T
+}
+
+export type PageInfo = {
+  hasPreviousPage: boolean
+  hasNextPage: boolean
+  startCursor: string|null
+  endCursor: string|null
+}
+
+export type Connection<T> = {
+  pageInfo: PageInfo
+  edges: Edge<T>[]
+}
+
+export enum PaginationOrder {
+  DESC = 0,
+  ASC = 1
+}
+
+export type PaginationConfig = {
+  client: MongoClient
+  params: ConnectionParams
+  orderField: string
+  order: PaginationOrder
+}
+
+export type paginate = <T extends Object>(config: PaginationConfig) => Promise<Connection<T>>
